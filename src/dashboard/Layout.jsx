09044124ff5151055d835dbdcdf0e5f34ec90d0a
Farukh Aleem <farukh.emeriosoft@gmail.com>
2024-12-28 20:38:27 +0500

    improvement

--- src/dashboard/Layout.jsx
+++ src/dashboard/Layout.jsx
@@ -178,7 +178,7 @@ export default function Layout({ content, title, addBtnTxt, addBtnPath }) {
 
                     <Menu as="div" className="relative inline-block text-left ">
                       <div className='mt-1'>
-                        <MenuButton  >
+                        <MenuButton className="notification-bell active" >
                           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="#d1d5db" className="size-7">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" />
                           </svg>

c32a1e1d0695146ea31a3da182f2503cfac31c48
Farukh Aleem <farukh.emeriosoft@gmail.com>
2024-12-30 14:48:00 +0500

    Login text updated

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -69,7 +69,7 @@ export default function Admin() {
             alt="Safehands International"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Admin Dashboard
+            Admin Login
           </h2>
         </div>
 
